5c3f5961be8a26e4d48afa02c90e61ec86a13030
ZykinaViktoriya <zykina.viktoriya@gmail.com>
2023-03-09 11:55:50 +0300

    new file

--- code/obj/Debug/net6.0/code.AssemblyInfo.cs
+++ code/obj/Debug/net6.0/code.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Final_project_GB")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Final_project_GB")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Final_project_GB")]
+[assembly: System.Reflection.AssemblyProductAttribute("code")]
+[assembly: System.Reflection.AssemblyTitleAttribute("code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
